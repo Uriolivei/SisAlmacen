229668636b41a6875a471b1b5be54281bea0064c
Uriolivei <1554602@senati.pe>
2024-10-07 08:59:49 -0500

    FULL

--- src/Entidades/Rol.java
+++ src/Entidades/Rol.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Entidades;
 
-/**
- *
- * @author SENATI
- */
 public class Rol {
+    private int idrol;
+    private String nombre;
+    private String descripcion;
+
+    public Rol() {
+        
+    }
+
+    public Rol(int idrol, String nombre, String descripcion) {
+        this.idrol = idrol;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public int getIdrol() {
+        return idrol;
+    }
+
+    public void setIdrol(int idrol) {
+        this.idrol = idrol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
     
 }
