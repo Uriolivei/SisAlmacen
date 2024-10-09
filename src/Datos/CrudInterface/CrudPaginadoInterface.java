511d6294f25648f1374078601e0eb87e9491c3da
Uriolivei <1554602@senati.pe>
2024-10-09 08:30:33 -0500

    full 1

--- src/Datos/CrudInterface/CrudPaginadoInterface.java
+++ src/Datos/CrudInterface/CrudPaginadoInterface.java
@@ -1,5 +1,13 @@
 package Datos.CrudInterface;
 
-public interface CrudPaginadoInterface {
-    
+import java.util.List;
+
+public interface CrudPaginadoInterface<T> {
+    public List<T> listar(String texto, int totalPorPagina, int numPagina);
+    public boolean insertar(T obj);
+    public boolean actualizar(T obj);
+    public boolean desactivar(int id);
+    public boolean activar(int id);
+    public int total();
+    public boolean existe(String texto);
 }
---
+++ src/Datos/UsuarioDAO.java
@@ -0,0 +1,44 @@
+package Datos;
+
+import Datos.CrudInterface.CrudPaginadoInterface;
+import Entidades.Usuario;
+import java.util.List;
+
+public class UsuarioDAO implements CrudPaginadoInterface<Usuario>{
+
+    @Override
+    public List<Usuario> listar(String texto, int totalPorPagina, int numPagina) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean insertar(Usuario obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean actualizar(Usuario obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean desactivar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean activar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int total() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean existe(String texto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
