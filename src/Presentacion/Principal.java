820aba261522ac39b351235839db9cc053d638f7
Uriolivei <1554602@senati.pe>
2024-10-10 10:45:50 -0900

    Menu Lateral

--- src/Presentacion/Principal.java
+++ src/Presentacion/Principal.java
@@ -5,25 +5,29 @@
 package Presentacion;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
-import javax.swing.border.EmptyBorder;
-
 /**
  *
  * @author SENATI
  */
 public class Principal extends javax.swing.JFrame {
-    
+     private JMenu activeMenu;
+     private final Map<JMenu, Color> menuColors = new HashMap<>();
     /**
      * Creates new form Principal
      */
@@ -78,15 +82,13 @@ public class Principal extends javax.swing.JFrame {
         menu1.setFont(font);
         item1.setFont(menufont);
         item2.setFont(menufont);
-        item1.setBorder(new EmptyBorder(5, 5, 5, 5)); 
-        item2.setBorder(new EmptyBorder(5, 5, 5, 5));
         item1.setAccelerator(KeyStroke.getKeyStroke('C', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+C
         item2.setAccelerator(KeyStroke.getKeyStroke('A', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+A
-
         item1.setIcon(icon11);
         item2.setIcon(icon21);
         menu1.setIcon(icon);
         menuBar1.add(menu1);
+        setupMenu(menu1, Color.ORANGE);
         sidePanel.add(menuBar1);
         
         item1.addActionListener(new ActionListener() {
@@ -112,7 +114,28 @@ public class Principal extends javax.swing.JFrame {
                 frmproductos.toFront();
             }
         });
+        
+        /*menu1.setForeground(Color.BLACK);
+        menu1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                // Solo cambia a naranja si no está seleccionado
+                menu1.setForeground(Color.BLACK); // Color al pasar el ratón
+            }
 
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // No hacer nada al salir, ya que se mantiene en naranja si se hizo clic
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Cambiar el color al hacer clic
+                menu1.setForeground(Color.ORANGE);
+            }
+        });*/
+
+        
         JMenu menu2 = new JMenu("Compras");
         JMenuItem item3 = new JMenuItem("Proveedores");
         JMenuItem item4 = new JMenuItem("Ingresos");
@@ -126,11 +149,10 @@ public class Principal extends javax.swing.JFrame {
         item4.setFont(menufont);
         item3.setIcon(icon31);
         item4.setIcon(icon41);
-        item3.setBorder(new EmptyBorder(5, 5, 5, 5)); 
-        item4.setBorder(new EmptyBorder(5, 5, 5, 5));
         item3.setAccelerator(KeyStroke.getKeyStroke('P', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+P
         item4.setAccelerator(KeyStroke.getKeyStroke('I', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+I
         menuBar2.add(menu2);
+        setupMenu(menu2, Color.GREEN);
         sidePanel.add(menuBar2);
         
         item3.addActionListener(new ActionListener() {
@@ -157,6 +179,26 @@ public class Principal extends javax.swing.JFrame {
             }
         });
         
+        /*menu2.setForeground(Color.BLACK);
+        menu2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                menu2.setForeground(Color.BLACK); // Color al pasar el ratón
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // Regresa al color negro al salir
+                //menu2.setForeground(Color.ORANGE);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Cambiar el color de menu1 a negro al hacer clic en menu2
+                menu1.setForeground(Color.ORANGE);
+            }
+        });*/
+        
         JMenu menu3 = new JMenu("Ventas");
         JMenuItem item5 = new JMenuItem("Clientes");
         JMenuItem item6 = new JMenuItem("Ventas");
@@ -170,11 +212,10 @@ public class Principal extends javax.swing.JFrame {
         item6.setFont(menufont);
         item5.setIcon(icon51);
         item6.setIcon(icon61);
-        item5.setBorder(new EmptyBorder(5, 5, 5, 5)); 
-        item6.setBorder(new EmptyBorder(5, 5, 5, 5));
         item5.setAccelerator(KeyStroke.getKeyStroke('L', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+L
         item6.setAccelerator(KeyStroke.getKeyStroke('V', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+V
         menuBar3.add(menu3);
+        setupMenu(menu3, Color.BLUE);
         sidePanel.add(menuBar3);
         
         item5.addActionListener(new ActionListener() {
@@ -201,6 +242,26 @@ public class Principal extends javax.swing.JFrame {
             }
         });
         
+        /*menu3.setForeground(Color.BLACK);
+        menu3.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                menu3.setForeground(Color.BLACK); // Color al pasar el ratón
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // Regresa al color negro al salir
+                //menu3.setForeground(Color.ORANGE);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Cambiar el color de menu1 a negro al hacer clic en menu2
+                menu2.setForeground(Color.ORANGE);
+            }
+        });*/
+        
         JMenu menu4 = new JMenu("Accesos");
         JMenuItem item7 = new JMenuItem("Roles");
         JMenuItem item8 = new JMenuItem("Usuarios");
@@ -214,11 +275,10 @@ public class Principal extends javax.swing.JFrame {
         item8.setFont(menufont);
         item7.setIcon(icon71);
         item8.setIcon(icon81);
-        item7.setBorder(new EmptyBorder(5, 5, 5, 5)); 
-        item8.setBorder(new EmptyBorder(5, 5, 5, 5));
         item7.setAccelerator(KeyStroke.getKeyStroke('R', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+R
         item8.setAccelerator(KeyStroke.getKeyStroke('U', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+U
         menuBar4.add(menu4);
+        setupMenu(menu4, Color.ORANGE);
         sidePanel.add(menuBar4);
         
         item7.addActionListener(new ActionListener() {
@@ -245,6 +305,26 @@ public class Principal extends javax.swing.JFrame {
             }
         });
         
+        /*menu4.setForeground(Color.BLACK);
+        menu4.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                menu4.setForeground(Color.BLACK); // Color al pasar el ratón
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // Regresa al color negro al salir
+                //menu4.setForeground(Color.ORANGE);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Cambiar el color de menu1 a negro al hacer clic en menu2
+                menu3.setForeground(Color.ORANGE);
+            }
+        });*/
+        
         JMenu menu5 = new JMenu("Consultas");
         JMenuItem item9 = new JMenuItem("Consultas Compras");
         JMenuItem item10 = new JMenuItem("Consultas Ventas");
@@ -258,11 +338,10 @@ public class Principal extends javax.swing.JFrame {
         item10.setFont(menufont);
         item9.setIcon(icon91);
         item10.setIcon(icon101);
-        item9.setBorder(new EmptyBorder(5, 5, 5, 5)); 
-        item10.setBorder(new EmptyBorder(5, 5, 5, 5));
         item9.setAccelerator(KeyStroke.getKeyStroke('O', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+O
         item10.setAccelerator(KeyStroke.getKeyStroke('N', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask())); // Ctrl+N
         menuBar5.add(menu5);
+        setupMenu(menu5, Color.GREEN);
         sidePanel.add(menuBar5);
         
         item9.addActionListener(new ActionListener() {
@@ -288,9 +367,54 @@ public class Principal extends javax.swing.JFrame {
                 frmconsultasventas.toFront();
             }
         });
+        
+        /*menu5.setForeground(Color.BLACK);
+        menu5.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                menu4.setForeground(Color.BLACK); // Color al pasar el ratón
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // Regresa al color negro al salir
+                //menu5.setForeground(Color.ORANGE);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Cambiar el color de menu1 a negro al hacer clic en menu2
+                menu5.setForeground(Color.ORANGE);
+            }
+        });*/
 
         getContentPane().add(sidePanel, BorderLayout.WEST);
+        
+    }
+    
+    private void setupMenu(JMenu menu, Color color) {
+        menu.setForeground(Color.BLACK);
+        menu.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                menu.setForeground(Color.BLACK); // Color al pasar el ratón
+            }
 
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // No se cambia el color al salir
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Cambiar el color al hacer clic
+                if (activeMenu != null) {
+                    activeMenu.setForeground(Color.BLACK); // Restablecer el color del menú anterior
+                }
+                menu.setForeground(color); // Cambiar el color del menú actual a naranja
+                activeMenu = menu; // Actualizar el menú activo
+            }
+        });
     }
     
     /**
@@ -349,7 +473,7 @@ public class Principal extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(0, 204, 51));
 
-        lblSalir.setFont(new java.awt.Font("Segoe UI", 0, 13)); // NOI18N
+        lblSalir.setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
         lblSalir.setForeground(new java.awt.Color(255, 255, 255));
         lblSalir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Presentacion/Imagenes/salir.png"))); // NOI18N
         lblSalir.setText("SALIR");
@@ -368,10 +492,10 @@ public class Principal extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(171, Short.MAX_VALUE)
+                .addContainerGap(159, Short.MAX_VALUE)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 536, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(99, 99, 99)
-                .addComponent(lblSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(101, 101, 101)
+                .addComponent(lblSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
